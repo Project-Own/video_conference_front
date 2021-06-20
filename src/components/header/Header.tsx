4bce351206172403e8bc085a2cb3d0b3d5f8c7ee
rohitjoey <roht.praz@gmail.com>
2021-06-20 14:35:55 +0545

    Changes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,65 +1,72 @@
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/icons/Menu';
+import loadable from "@loadable/component";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { addURLPath } from "../../utils/utils";
 import { Loading } from "../Loading/Loading";
-import loadable from "@loadable/component";
-
-
-
 
 const LoadableHost = loadable(() => import("../host/Host"), {
-    fallback: <Loading />,
-  });
+  fallback: <Loading />,
+});
+
+// const LoadableView = loadable(() => import("../conferenceGridView"), {
+//   fallback: <Loading />,
+// });
 
 const Header = () => {
-return (
-<Router>
-    <div>
+  return (
+    <Router>
+      <div>
         <Toolbar>
-            <Grid container direction="row-reverse" >
-                <Grid item xs={1}>
-                    <Typography variant="h6">
-                        <MenuIcon/>
-                    </Typography>
-                </Grid>
-
-                <Grid item xs={1}>
-                    <Typography variant="h6">
-                        <Link to={addURLPath("/team")}>Team</Link>
-                    </Typography>
-                </Grid>
-
-                <Grid item xs={1}>
-                    <Typography variant="h6">
-                        <Link to={addURLPath("/about")}>About</Link>
-                    </Typography>
-                </Grid>
-
-                <Grid item xs={1}>
-                    <Typography variant="h6">
-                        <Link to={addURLPath("/")}>Home</Link>
-                    </Typography>
-                </Grid>
+          <Grid container direction="row-reverse">
+            <Grid item xs={1}>
+              <Typography variant="h6">
+                <MenuIcon />
+              </Typography>
             </Grid>
+
+            {/* <Grid item xs={1}>
+              <Typography variant="h6">
+                <Link to={addURLPath("/view")}>view</Link>
+              </Typography>
+            </Grid> */}
+
+            <Grid item xs={1}>
+              <Typography variant="h6">
+                <Link to={addURLPath("/about")}>About</Link>
+              </Typography>
+            </Grid>
+            <Grid item xs={1}>
+              <Typography variant="h6">
+                <Link to={addURLPath("/team")}>Team</Link>
+              </Typography>
+            </Grid>
+            <Grid item xs={1}>
+              <Typography variant="h6">
+                <Link to={addURLPath("/")}>Home</Link>
+              </Typography>
+            </Grid>
+          </Grid>
         </Toolbar>
 
         <Switch>
-            <Route exact path={addURLPath("/")}>
-            </Route>
+          <Route exact path={addURLPath("/")}></Route>
 
-            <Route path={addURLPath("/about")}>
-                <LoadableHost />
-            </Route>
+          <Route path={addURLPath("/about")}>
+            <LoadableHost />
+          </Route>
+          {/* 
+          <Route path={addURLPath("/view")}>
+            <LoadableView />
+          </Route> */}
 
-            <Route path={addURLPath("/team")}>
-            </Route>
+          <Route path={addURLPath("/team")}></Route>
         </Switch>
-    </div>
-</Router>
-    )
-}
+      </div>
+    </Router>
+  );
+};
 
-export default Header
+export default Header;
